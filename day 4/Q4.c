f08ec757000823dd301787859b2be18e8dc3fa0d
sriharikrish0611-code <sriharikrish0611@gmail.com>
2025-10-28 20:38:53 +0530

    Add files via upload

---
+++ day 4/Q4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b && a>c)
+	{
+		printf("%d",a);
+	}
+	else if(b>a && b>c){
+		printf("%d",b);
+	}
+	else{
+		printf("%d",c);
+	}}
